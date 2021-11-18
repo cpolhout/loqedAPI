7228631a93c902190b3025b54b1997ddf6663f87
cpolhout <casper@onegini.com>
2021-11-18 01:30:37 +0100

    CHanged name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='loqedAPI',
+    name='pyloqed',
     version='0.1.0',    
     description='Python package to use the Loqed Smart Door Lock APIs. To be used by Home Assistant.',
     url='https://github.com/cpolhout/loqedAPI',
