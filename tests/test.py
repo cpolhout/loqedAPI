26b7a31777355f0d8b23107e3fb09aba80b6e34c
cpolhout <casper@onegini.com>
2022-03-27 01:15:40 +0100

    Updated to local API

--- tests/test.py
+++ tests/test.py
@@ -1,27 +1,40 @@
 import asyncio
 import aiohttp
 import logging
+import os
+print('cwd is %s' %(os.getcwd()))
 
-from loqedAPI import APIClient
-from loqedAPI import LoqedAPI
-from loqedAPI import Lock
+from loqedAPI.loqed import APIClient
+from loqedAPI.loqed import LoqedAPI
+from loqedAPI.loqed import Lock
 
 logging.basicConfig(level=logging.DEBUG)
 
 async def main():
     async with aiohttp.ClientSession() as session:
         #apiclient = APIClient(session, "https://bc615891-1f7d-4237-8b72-f20e5719d50e.mock.pstmn.io/api", "")
-        apiclient = APIClient(session, "https://integrations.production.loqed.com/api", "xxx")
+        apiclient = APIClient(session, "http://10.0.0.104")
         api = LoqedAPI(apiclient)
-        locks = await api.async_get_locks()
-        print(f"The lock name: {locks[0].name}")
-        print(f"The lock ID: {locks[0].id}")
+        secret='VUoI/HmtiN7WYTnXkB/NdiGaICC60KFz9OxtnJ4ebHY='
+        bridgekey='Go6L8l83Lf7oGMhaBCbDzwNTOUmfKPCS9uN0a1KfPz4='
+        lock = await api.async_get_lock(secret,bridgekey, "Polhuiss")
+        print(f"The lock name: {lock.name}")
+        print(f"The lock ID: {lock.id}")
+        print(f"Locking the lock...")
+        # await lock.unlock()
+
         print(f"Registering dummy hook..")
-        locks[0].registerWebhook("https://www.polhout.me/webhook/1212312121")
+        json_data=await lock.getWebhooks()
+        for hook in json_data:
+            print("FOUND WEBHOOK:" + str(hook))
 
-        print("Opening lock")
+        status=await lock.registerWebhook("https://webhook.site/162bed3b-445e-4571-8b44-4a3f132961a4")
+        print(status)
 
-       # await locks[0].open()
+        json_data=await lock.getWebhooks()
+        for hook in json_data:
+            print("FOUND WEBHOOK:" + str(hook))
+
 
 
 asyncio.run(main())
